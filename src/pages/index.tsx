04c5ee59ad8dd832b4d9f848458ed02c80222f71
Rodrigo Andrade <randrade@brbatel.com.br>
2021-04-02 21:16:47 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { stripe } from '../services/stripe';
 
 interface HomeProps {
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IbwMxICUlfRiYZQH276fikb', {
     expand: ['product']
   })
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
